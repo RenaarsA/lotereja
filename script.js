a75a6b26f191cb123de325f4ed0d285de1c7d4b1
RenaarsA <DogieLV@Gmail.com>
2021-11-29 11:01:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert(1)
+const vardi = ['Horace Slughorn','Tom Riddle','Ron Weasley','Albus Dumbeldor'];
+const balvas = ['Sorcerer stone','Resurection stone','Elder Wand','Invisibilty cloak,'];
+const naudaKopa= 1000000;
+let uzvaretajuSkaits=3;
+
+Math.random=Math.random()*10;
