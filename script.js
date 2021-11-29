8acbf077235668805d29b0326453cbdf0bd86c14
RenaarsA <DogieLV@Gmail.com>
2021-11-29 12:14:47 +0200

    lotereja

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const vardi = ['Horace Slughorn','Tom Riddle','Ron Weasley','Albus Dumbeldor'];
-const balvas = ['Sorcerer stone','Resurection stone','Elder Wand','Invisibilty cloak,'];
-const naudaKopa= 1000000;
-let uzvaretajuSkaits=3;
+const vardi = ['Horace Slughorn', 'Tom Riddle', 'Ron Weasley', 'Albus Dumbledore'];
+const balvas = ['Sorcerer stone', 'Resurection stone', 'Elder Wand', 'Invisibilty cloak,'];
+const naudaKopa = 1000000;
+let uzvaretajuSkaits = 3;
+let rindas = document.querySelector('.rindas');//pievieno mainīgo
 
-Math.random=Math.random()*10;
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.length;//vārdu skaits mainīgs
+    rand = Math.floor(rand);//noapaļo uz leju
+    console.log(vardi[rand]);
+
+
+    let uzvaretajs = vardi[rand];// console.log vietā
+    rindas.innerHTML += `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${uzvaretajs}</td>
+    </tr>`
+
+
+
+
+    //index.html meta cods nozīmē kad atļautas ir garumzīme
+}
